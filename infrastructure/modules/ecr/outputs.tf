591fba76c5dd63415966e465b701ea92fa0ea607
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-09-12 11:32:44 +0100

    test pipeline

--- infrastructure/modules/ecr/outputs.tf
+++ infrastructure/modules/ecr/outputs.tf
@@ -1,9 +1,7 @@
 output "repository_url" {
-  description = "The URL of the ECR repository"
   value       = aws_ecr_repository.this.repository_url
 }
 
 output "repository_arn" {
-  description = "The ARN of the ECR repository"
   value       = aws_ecr_repository.this.arn
 }
