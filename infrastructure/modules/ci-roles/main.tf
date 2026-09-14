a5c44eba278d9405f2a23ff612fa698e23d4177f
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-09-14 13:10:56 +0100

    update terraform_plan_readonly_policy

--- infrastructure/modules/ci-roles/main.tf
+++ infrastructure/modules/ci-roles/main.tf
@@ -59,7 +59,9 @@ resource "aws_iam_role_policy" "terraform_plan_readonly_policy" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:DeleteObject"
         ]
         Resource = [
           var.state_bucket_arn,
