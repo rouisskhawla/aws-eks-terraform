a6659a47dab68abe02585921a7e3ba9c12c8fd7f
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-09-14 13:45:23 +0100

    add S3LockFile permissions to terraform_plan_readonly_policy

--- infrastructure/modules/ci-roles/main.tf
+++ infrastructure/modules/ci-roles/main.tf
@@ -60,13 +60,21 @@ resource "aws_iam_role_policy" "terraform_plan_readonly_policy" {
         Action = [
           "s3:GetObject",
           "s3:ListBucket",
-          "s3:PutObject",
-          "s3:DeleteObject"
         ]
         Resource = [
           var.state_bucket_arn,
           "${var.state_bucket_arn}/*",
         ]
+      },
+      {
+        Sid    = "S3LockFile"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${var.state_bucket_arn}/terraform.tfstate.tflock"
       }
     ]
   })
