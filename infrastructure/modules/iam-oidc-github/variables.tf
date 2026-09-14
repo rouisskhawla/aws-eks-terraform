1e0d52ffd962d3a4b9af7f8fbb3041885e1443b8
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-09-14 13:02:29 +0100

    upgraded workflow's terraform version so it matches local one

--- infrastructure/modules/iam-oidc-github/variables.tf
+++ infrastructure/modules/iam-oidc-github/variables.tf
@@ -6,7 +6,6 @@ variable "role_name" {
 variable "github_repo" {
   type    = string
   default = "rouisskhawla@38538299/aws-eks-terraform@1364055098"
-  description = "GitHub repository in the format 'owner/repo'."
 }
 
 variable "github_branch" {
